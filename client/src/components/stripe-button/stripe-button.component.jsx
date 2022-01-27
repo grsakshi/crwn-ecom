f153cf48d584e18b4890565fcf2d8317cf5170c8
Sakshi Gairola <72180456+grsakshi@users.noreply.github.com>
2022-01-27 21:19:09 +0530

    merge (#4)
    * separating our cra app into client folder and added express server to handle payments
    
    * Update README.md
    
    * adding server code to repo

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 import './strip.styles.css';
 //stripe wants price in cents not usd
@@ -8,8 +9,21 @@ const StripeCheckoutButton = ({price}) => {
     const publishableKey = 'pk_test_51IZGTBSDWky9aSMXUOm8usq6ZmsA5NuTU1gN73mmY9UjQ8rf5HstvWBRoqgzckW84ZZzsLY8Ongjjh0MiF7RA5o600NTWJg0Po';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+        axios({
+            url: 'http://localhost:5000/payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        })
+        .then(response => {
+            alert("Payment Successful");
+        })
+        .catch(error => {
+            console.log('Payment Error: ', error);
+            alert("There was an issue with your payment. Please make sure you use the provided credit card.")
+        });
     }
 
     return(
